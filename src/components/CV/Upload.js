f6b0f564d3f52a3e52bf00b370f9400b9e931530
Vincent-Baret1 <vincent.baret@etu.umontpellier.fr>
2022-11-13 18:24:54 +0100

    feat(): add complete frontend

--- src/components/CV/Upload.js
+++ src/components/CV/Upload.js
@@ -1,4 +1,5 @@
 import Button from '@mui/material/Button';
+import { TextField } from '@mui/material';
 import axios from 'axios';
 
 const Upload = () => {
@@ -10,19 +11,32 @@ const Upload = () => {
         const file = event.target.files[0];
         formData.append("file", file);
 
-        axios.post(`http://localhost:8080/cv-upload`, formData)   
-        .then(res => {
-          console.log(res);
-          console.log(res.data);
-        })
-    }    
-        
+        axios.post(`http://localhost:8080/cv-upload`, formData)
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            })
+    }
+
 
     return (
-        <Button variant="contained" component="label">
-        Upload
-        <input onChange={handleChangeFile} hidden accept="*" multiple type="file" />
-      </Button>
+        <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+        }}>
+            <div style={{marginTop: '8em'}}>
+                <Button style={{ alignSelf: 'center' }} variant="contained" component="label">
+                    Importer mon CV
+                    <input onChange={handleChangeFile} hidden accept="*" multiple type="file" />
+                </Button>
+                <p style={{ color: 'grey', fontSize: '15px', marginTop: '30px'}}>Fichier PDF uniquement</p>
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '40px'}}>
+                <p style={{ fontSize: '20px', width: '60%'}}>Pour pouvoir obtenir des offres d'emploi pertinentes, veuillez choisir <strong>3 mots-clés</strong> qui définissent au mieux votre CV :</p>
+                <TextField style={{ marginTop: '30px'}} id="outlined-basic" label="Mot-clé" variant="outlined" />
+            </div>
+        </div>
     )
 }
 
