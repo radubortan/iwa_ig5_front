d521a8c5d5ee38fec0502f082388b0c5b959dbed
Vincent-Baret1 <vincent.baret@etu.umontpellier.fr>
2022-11-18 23:12:07 +0100

    feat(): add view CV possibility

--- src/components/CV/Upload.js
+++ src/components/CV/Upload.js
@@ -4,12 +4,21 @@ import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Upload = () => {
 
     const [loading, setLoading] = useState(false);
     const [uploaded, setUploaded] = useState(false);
+    const [displayError, setDisplayError] = useState(false);
+
+    //If a job seeker already uploaded a CV
+    const [cvExist, setCvExist] = useState(false);
+    const [fileURLCV, setFileURLCV] = useState();
+
+    useEffect(() => {
+        fetchCV("CV_Vincent_Baret.pdf");
+    })
 
     const handleChangeFile = (event) => {
         event.preventDefault();
@@ -21,12 +30,32 @@ const Upload = () => {
 
         axios.post(`http://localhost:8080/cv-upload`, formData)
             .then(res => {
-                console.log(res);
-                console.log(res.data);
-                if(res.data) {
+                if (res.data) {
                     setLoading(false);
+                    setDisplayError(false);
                     setUploaded(true);
+                    setCvExist(true);
                 }
+            })
+            .catch(err => {
+                setLoading(false);
+                setDisplayError(true);
+            })
+    }
+
+    function fetchCV(fileName){
+        axios.get(`http://localhost:8080/cv-view?fileName=${fileName}`, {responseType: 'blob'})
+            .then(res => {
+                const blob = new Blob(
+                    [res.data],
+                    {type: 'application/pdf'}
+                );
+                var fileURL = URL.createObjectURL(blob);
+                setFileURLCV(fileURL);
+                setCvExist(true);
+            })
+            .catch(err => {
+                console.log(err);
             })
     }
 
@@ -37,7 +66,7 @@ const Upload = () => {
                 <CircularProgress />
             </Box>
         )
-    } else {
+    } else if (cvExist === false) {
         return (
             <div style={{
                 display: 'flex',
@@ -48,13 +77,28 @@ const Upload = () => {
                     {uploaded === false ? <Button variant="contained" component="label">
                         Importer mon CV
                         <input onChange={handleChangeFile} hidden accept="*" multiple type="file" />
-                    </Button> : <p style={{color: 'green', fontSize: '20px'}}>Le téléchargement de votre CV a réussi !</p>} 
+                    </Button> : <p style={{ color: 'green', fontSize: '20px' }}>Le téléchargement de votre CV a réussi !</p>}
+
+                    {displayError === true && <p style={{ color: 'red', fontSize: '20px' }}>Une erreur est survenue, merci de réessayer.</p>}
+
                     <p style={{ color: 'grey', fontSize: '15px', marginTop: '30px' }}>Fichier PDF uniquement</p>
                 </div>
+
                 <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '40px' }}>
                     <p style={{ fontSize: '20px', width: '60%' }}>Pour pouvoir obtenir des offres d'emploi pertinentes, veuillez choisir <strong>3 mots-clés</strong> qui définissent au mieux votre CV :</p>
                     <TextField style={{ marginTop: '30px' }} id="outlined-basic" label="Mot-clé" variant="outlined" />
                 </div>
+
+            </div>
+        )
+    } else {
+        return (
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center'
+            }}>
+                <Button onClick={() => {window.open(fileURLCV)}} style={{ marginTop: '8em' }} variant="contained" component="label">Voir mon CV</Button>
             </div>
         )
     }
